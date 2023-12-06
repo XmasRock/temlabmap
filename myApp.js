6163d6ae044d804953d6dd996118e0994b15a8dd
wtfproject2023 <walkingthefutureproject@gmail.com>
2023-12-06 23:46:38 +0100

    fix slug

--- myApp.js
+++ myApp.js
@@ -2,12 +2,13 @@
 
 
 const map = L.map('map').setView([46.726229017579755, 2.428144757082328], 6);
+const slugs=[];
 
 const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 25,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-afficherInstallations(InstallationsData, map);
+afficherInstallations(InstallationsData, map,slugs);
 
 
--- myAppFunctions.js
+++ myAppFunctions.js
@@ -12,14 +12,17 @@ const slugify = (text) => {
     .replace(/\-$/g, '');         // Remove trailing -
 }
 
-function sendMessage(in_id, in_slug) {
-  const message = ""+in_id+"/"+in_slug;
+function sendMessage(in_id, in_slug_id, in_slugs) {
+  const message = ""+in_id+"/"+in_slugs[in_slug_id];
   const targetOrigin = 'https://temlab.fr';
   parent.postMessage(message, targetOrigin);
   }
 
-function afficherInstallations(in_installations, in_map){
+function afficherInstallations(in_installations, in_map, in_slugs){
+  slug_id = 0;
   in_installations.forEach(element => {
-    marker = L.marker(element.coord).bindPopup('<table width="300px" height="150px"><tr><td align="center" valign="middle"  style="font-size: 14px"><img src="'+element.image+'" height="120px" width="120px"/><br/><b>'+element.societe+'</b></td><td  align="center" valign="middle" style="font-size: 14px"><b><a href="" onclick="sendMessage('+element.ID+','+slugify(element.titre)+');">'+element.titre+'</a></b><br/><br/>'+element.adresse+'<br/><br/><p>'+element.resume+'</p></td></tr></table>').addTo(in_map);
+    in_slugs.push(slugify(element.titre));
+    marker = L.marker(element.coord).bindPopup('<table width="300px" height="150px"><tr><td align="center" valign="middle"  style="font-size: 14px"><img src="'+element.image+'" height="120px" width="120px"/><br/><b>'+element.societe+'</b></td><td  align="center" valign="middle" style="font-size: 14px"><b><a href="" onclick="sendMessage('+element.ID+','+slug_id+',in_slugs);">'+element.titre+'</a></b><br/><br/>'+element.adresse+'<br/><br/><p>'+element.resume+'</p></td></tr></table>').addTo(in_map);
+    slug_id++;
   });
 }
